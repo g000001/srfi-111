79a61347d030b4c702d7bd9922af95891253776b
g000001 <chiba.masaomi@gmail.com>
2013-10-16 23:52:50 +0900

    fix: compile time definition

--- srfi-111.lisp
+++ srfi-111.lisp
@@ -1,12 +1,12 @@
 ;;;; srfi-111.lisp
 (cl:in-package :srfi-111.internal)
 
-
-(cl:defstruct (box (:constructor box (obj))
-                   (:predicate box?)
-                   (:print-object (cl:lambda (obj stream)
-                                    (cl:format stream "#&~S" (unbox obj)))))
-  obj)
+(cl:eval-when (:compile-toplevel :load-toplevel :execute)
+  (cl:defstruct (box (:constructor box (obj))
+                     (:predicate box?)
+                     (:print-object (cl:lambda (obj stream)
+                                      (cl:format stream "#&~S" (unbox obj)))))
+    obj))
 
 
 (cl:defmethod cl:make-load-form ((self box) cl:&optional env)
